010fb3335f9664b4c4c8c22939362f6d9ea36e16
Kostiuk93 <kostiuk93@gmail.com>
2022-04-30 13:22:42 +0300

    React Basic элементы и компоненты

--- src/index.js
+++ src/index.js
@@ -2,26 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-                                //тег       имя класса             текст
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!'); // JS
-
-const text = 'Hello World'
-
-const elem = (
-  // Должен иметь хотя бы одного родителя
-  // class и for зарезервированы системой.
-  // Необходимо использовать className и htmlFor соответственно
-  // в {} можно помещать все, кроме объектов
-  <div> 
-    <h2 className='text'>{text}</h2> 
-    <input type="text" />
-    <label htmlFor=''></label>
-    <button tabIndex={0}>Click</button>
-  </div>
-); // JSX
+import { Header } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App/> // React компонент. Всегда пишется с большой буквы
 );
