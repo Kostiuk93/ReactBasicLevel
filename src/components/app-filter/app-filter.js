fae89f662cec54d99d03af01b846e4bdfb667bd1
Kostiuk93 <kostiuk93@gmail.com>
2022-05-15 17:16:58 +0300

    React Basic Динамические классы и стили

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -2,20 +2,22 @@ import './app-filter.css'
 
 const AppFilter = (props) => {
     const buttonsData = [
-        {name: 'all', label: 'Все сотрудники'},
-        {name: 'rise', label: 'На повышении'},
-        {name: 'moreThen1000', label: 'З/П больше 1000$'}
+        {name: 'all', label: 'Все сотрудники', colored: false},
+        {name: 'rise', label: 'На повышении', colored: true},
+        {name: 'moreThen1000', label: 'З/П больше 1000$', colored: false}
     ]
 
-    const buttons = buttonsData.map(({name, label}) => {
+    const buttons = buttonsData.map(({name, label, colored}) => {
         const active = props.filter === name
         const clazz = active ? 'btn-light' : 'btn-outline-light'
+        const styleVariable = colored ? {color: 'red'} : null
         return (
             <button 
             className={`btn ${clazz}`}
             type="button"
             key = {name}
             onClick = {() => props.onFilterSelect(name)}
+            style = {styleVariable}
             >
 
             {label}
